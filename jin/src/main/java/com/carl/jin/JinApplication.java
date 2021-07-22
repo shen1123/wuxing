b3dae4016cb56f1a0b13da6421c2690557e4649a
carl.shi <carl.shi@kedongnet.com>
2021-07-22 10:06:11 +0800

    初步搭建es,mq

--- jin/src/main/java/com/carl/jin/JinApplication.java
+++ jin/src/main/java/com/carl/jin/JinApplication.java
@@ -2,7 +2,12 @@ package com.carl.jin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class JinApplication {
 
--- tu/src/main/java/com/carl/tu/TuApplication.java
+++ tu/src/main/java/com/carl/tu/TuApplication.java
@@ -3,11 +3,12 @@ package com.carl.tu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages = {"com.carl"})
-@EnableEurekaClient
+@EnableDiscoveryClient
 @MapperScan("com.carl.tu.dao")
 @SpringBootApplication(scanBasePackages = "com.carl")
 public class TuApplication {
